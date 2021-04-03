50200b4777d6076b2fd384c35ff05ae3a8d68fd5
unknown <aromero@aeonlinesolutions.com>
2021-04-03 16:17:47 -0500

    se agrega uevo index

---
+++ 02-intro-js/src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+// variales y contanstes
+const nombre = 'arturo';
+const apellido = 'romero';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+// var no se debe usar
+if (true) {
+    const nombre = 'peter'
+    console.log(nombre);
+}
+
+console.log(valorDado);
