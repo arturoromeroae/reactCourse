9d961db5ee7f958c8215867231b5b21d5c29f4f2
unknown <aromero@aeonlinesolutions.com>
2021-04-07 17:19:21 -0500

    se realiza tarea

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
+// import PrimeraApp from './PrimeraApp';
+
 import './index.css';
 
 const divRoot = document.querySelector('#root');
 
 // ReactDom.render(<PrimeraApp saludo = "Hola es mi primera app" />, divRoot);
 // ReactDom.render(<PrimeraApp saludo = {123} />, divRoot);
-ReactDom.render(<PrimeraApp saludo = "Hola Mundo" />, divRoot);
+// ReactDom.render(<PrimeraApp saludo = "Hola Mundo" />, divRoot);
+ReactDom.render(<CounterApp value = {123} />, divRoot);
