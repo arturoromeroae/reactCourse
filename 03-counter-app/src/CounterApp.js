0ebb16641e3142fafb26bd498ef32e954ea9e4f4
unknown <aromero@aeonlinesolutions.com>
2021-04-09 16:43:24 -0500

    curso

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
+
+    const state = useState();
+
+    // handleAdd
+    const handleAdd = (e) => {
+        console.log(e)
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
             <p>{value}</p>
+
+            <button onClick = {handleAdd}>+1</button>
         </>
 
     );
